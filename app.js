16d294e93c783ad9f22025ff7479843620a8e246
Sarad3993 <risingstar3993@gmail.com>
2023-02-25 22:39:44 +0545

    controller setup

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+
+
+// controller
+const stripeController = require('./controllers/stripeController');
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+
+// stripe 
+app.post('/stripe', stripeController)
 
 
 const port = process.env.PORT || 3000;
